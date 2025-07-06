ff402c64be5ab22f33700c47100d50148c09d898
Hai (Michael) Nguyen <phihai91@outlook.com>
2025-07-06 15:09:31 +0700

    [Config] #BE Init framework

---
+++ OpenSchool API/src/test/java/com/getopenschool/openschoolapi/OpenSchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getopenschool.openschoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSchoolApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
