babb3d982a15f73e22a2d21020e18662ea5ed1e4
pqhuy <pqhuy.java@gmail.com>
2025-08-10 00:11:06 +0700

    [fix] resolve mixed Content issue with swagger

--- backend/infrastructure/src/main/java/com/openschool/infrastructure/config/SwaggerConfig.java
+++ backend/infrastructure/src/main/java/com/openschool/infrastructure/config/SwaggerConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -17,6 +20,8 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info().title("OpenSchool API").version("v1"))
+                .servers(List.of(new Server().url("/")
+                ))
                 .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
                 .components(new Components().addSecuritySchemes(SECURITY_SCHEME_NAME,
                         new SecurityScheme()
